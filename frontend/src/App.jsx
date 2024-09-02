01cbdefdae9ebfec3e17a1f605d7a0058ab9b4a9
Aman Kumar <aman228115918@gmail.com>
2024-09-02 18:26:51 +0530

    new frontend setup done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="font-extrabold text-5xl text-blue-700 text-center ">
+        React App
+      </h1>
+    </>
+  );
+}
+
+export default App;
