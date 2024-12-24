ba6c2f34eb4602b9fbdc388145d29f1da0eee002
Aman Kumar <aman228115918@gmail.com>
2024-12-24 21:25:48 +0530

    deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,8 +5,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   server: {
     proxy: {
-      // "/api": "https://instavibe-backend-0u8r.onrender.com",
-      "/api": "http://localhost:8000",
+      "/api": "https://instavibe-backend-0u8r.onrender.com",
     },
   },
   plugins: [react()],
